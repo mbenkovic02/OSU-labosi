56c9f5e5db81c146da5396befe57237937ffa4c3
Mato Benković <156119917+mbenkovic02@users.noreply.github.com>
2024-05-20 04:10:43 +0200

    Update Zadatak_2_template.py

--- lv7/Zadatak_2_template.py
+++ lv7/Zadatak_2_template.py
@@ -43,3 +43,72 @@ for i in range(1, 7):
     axarr[1].imshow(img_array_aprox)
     plt.tight_layout()
     plt.show()
+
+
+
+##########
+
+
+
+
+    data_new = data_pd.drop(columns=['target_names'])
+
+data_new.info()
+
+wcss = []  # within-cluster sums of squares
+
+for i in range(1, 11):
+    kmeans = KMeans(n_clusters=i, init='k-means++', max_iter=300, n_init=10, random_state=0)
+    kmeans.fit(data_new)
+    wcss.append(kmeans.inertia_)
+
+
+plt.figure(figsize=(10, 6))
+plt.plot(range(1, 11), wcss, marker='o', linestyle='--')
+plt.title('Lakat metoda')
+plt.xlabel('Broj klastera')
+plt.ylabel('WCSS')
+plt.show()
+
+
+
+kmeans = KMeans(n_clusters=3, init='k-means++', max_iter=300, n_init=10, random_state=0)
+clusters = kmeans.fit_predict(data_new)
+
+data_new['cluster'] = clusters
+
+data_pd['cluster'] = clusters
+
+# Boje za različite klastere
+colors = {0: 'red', 1: 'green', 2: 'blue'}
+cluster_colors = data_pd['cluster'].map(colors)
+
+# Raspršeni dijagram
+plt.figure(figsize=(10, 6))
+plt.scatter(data_pd['petal length (cm)'], data_pd['petal width (cm)'], c=cluster_colors, label='Clusters', alpha=0.6)
+plt.title('Klasteri cvijeta irisa')
+plt.xlabel('Duljina latica (cm)')
+plt.ylabel('Širina latica (cm)')
+plt.legend()
+plt.show()
+
+
+# Mapiranje klastera na originalne klase
+def map_cluster_to_target(cluster_label):
+    if cluster_label == 0:
+        return 2  # virginica
+    elif cluster_label == 1:
+        return 0  # setosa
+    else:
+        return 1  # versicolor
+
+data_pd['cluster_mapped'] = data_pd['cluster'].map(map_cluster_to_target)
+
+
+
+# Izračunavanje preciznosti
+accuracy = accuracy_score(data_pd['target'], data_pd['cluster_mapped'])
+conf_matrix = confusion_matrix(data_pd['target'], data_pd['cluster_mapped'])
+
+print(f'Preciznost klasifikacije: {accuracy}')
+print(f'Matrici konfuzije:\n{conf_matrix}')
